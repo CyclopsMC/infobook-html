54ca3f99fc433c89f69c8310e61c8bf22feea02c
Ruben Taelman <rubensworks@gmail.com>
2019-05-27 19:43:19 +0200

    Run linter on bin/

--- bin/generate-infobook-html.ts
+++ bin/generate-infobook-html.ts
@@ -1,11 +1,11 @@
-import minimist = require("minimist");
 import * as fs from "fs";
-import {InfoBookInitializer} from "../lib/infobook/InfoBookInitializer";
+import minimist = require("minimist");
 import {IInfoBook} from "../lib/infobook/IInfoBook";
+import {InfoBookInitializer} from "../lib/infobook/InfoBookInitializer";
 
 // Process CLI args
 const args = minimist(process.argv.slice(2));
-if (args.help || args._.length != 1) {
+if (args.help || args._.length !== 1) {
   process.stdout.write(`generate-cyclops-infobook-html Output Cyclops infobooks as HTML
 Usage:
   generate-cyclops-infobook-html /path/to/config.json
@@ -20,8 +20,8 @@ async function create() {
   const config = JSON.parse(fs.readFileSync(args._[0], "utf8"));
   const infoBookInitializer = new InfoBookInitializer(config);
   const infoBook: IInfoBook = await infoBookInitializer.initialize();
-  console.log(JSON.stringify(infoBook, null, "  ")); // TODO
-  console.log(infoBook); // TODO
+  // console.log(JSON.stringify(infoBook, null, "  ")); // TODO
+  // console.log(infoBook); // TODO
 
 // Convert info book to HTML
 // TODO
